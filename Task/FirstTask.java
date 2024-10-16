edf869f3ed1faded2248ef5b0a934efb3c80099d
Natalia <natmizueva@gmail.com>
2024-10-16 21:04:10 +0300

    final_1

--- Task/FirstTask.java
+++ Task/FirstTask.java
@@ -1,24 +1,57 @@
 package Task;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class FirstTask {
     public static void main(String[] args) {
-        System.out.println("Калькулятор запустился.");
-        var x = List.of("картошка", "ветер", "мука", "ёмкость", "нора");
+        System.out.println("Игра Виселица началаѝь! У Вас есть 5 жизней.");
+        
+        var x = List.of("else", "live", "dog", "yoga");
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        var lives = 5;
+
         Random randomizer = new Random();
         String random = x.get(randomizer.nextInt(x.size()));
-        var lives = 5;
-        var guessed = 0;
-        while(lives > 0 || guessed < x.size()){
+        //System.out.println(random);
+        
+        while(lives > 0 && arrayList.size() < x.size()){
             System.out.println("Введите букву:");
-            Scanner s = new Scanner(System.in);
+            Scanner s = new Scanner(System.in, "UTF-8");
             char a = s.next().charAt(0);
-            System.out.println("Вы ввели букву:" + a);
+            String sc = "";
+            
+            if (random.indexOf(a) > -1){
+                System.out.println("Угадали! Осталось " + lives + " жизней.");
+                for(int k = 0; k < random.length(); k++){
+                    if(a == random.charAt(k)){
+                        arrayList.add(k);
+                    }
+                }      
+            }
+            else{
+                lives--;
+                System.out.println("Неверно! Осталось " + lives + " жизней.");
+            }
+            for(int t = 0; t < random.length(); t++){
+                if (arrayList.contains(t)){
+                    sc = sc + random.charAt(t);
+                }
+                else{
+                    sc = sc + "_";
+                }
+            }
+            System.out.println(sc);
         }
         
+        System.out.println("Игра окончена!");
+        if (lives == 0){ 
+            System.out.println("Вы проиграли!");
+        }
+        else{
+            System.out.println("Вы победили!");
+        }
     }
-
 }
