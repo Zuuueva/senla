ab958903950c71034df75afcf07c200f36896e70
Natalia <natmizueva@gmail.com>
2024-10-17 00:52:36 +0300

    final_3

--- Task/FirdTask.java
+++ Task/FirdTask.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class FirdTask {
     public static void main(String[] args) {
         System.out.println("Введите длину пароля от 8 до 12 символов:");
-            Scanner s = new Scanner(System.in, "UTF-8");
+            Scanner s = new Scanner(System.in);
             int length = s.nextInt();
         if (length >= 8 && length <= 12){
-            final String characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*_=+-/.?<>~)";
+            String characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*_=+-/.?<>~)";
             SecureRandom rand = new SecureRandom();
             StringBuilder stringbuild = new StringBuilder();
             for (int i = 0; i < length; i++)
--- Task/SecondTask.java
+++ Task/SecondTask.java
@@ -4,10 +4,51 @@ import java.util.Scanner;
 
 public class SecondTask {
     public static void main(String[] args) {
-        System.out.println("Выбирите длину пароля от 8 до 12 символов: ");
-        Scanner s = new Scanner(System.in, "UTF-8");
-            char a = s.next().charAt(0);
-        
+        System.out.println("1 Ruble");
+        System.out.println("2 Dollar");
+        System.out.println("3 Euro");
+        System.out.println("4 Yuan");
+        System.out.println("5 Lyra");
+        Scanner sc = new Scanner(System.in);
+        System. out.println("Выберите валюту:");
+        int choice = sc.nextInt();
+        System.out.println("Введите сумму:");
+        double amount = sc.nextDouble();
+        // convert the amount
+        switch (choice) {
+            case 1:
+                System.out.println(amount + " Ruble = " + (amount * 0.010282) + " Dollar");
+                System.out.println(amount + " Ruble = " + (amount * 0.009426) + " Euro");
+                System.out.println(amount + " Ruble = " + (amount * 0.073574) + " Yuan");
+                System.out.println(amount + " Ruble = " + (amount * 0.351981) + " Lyra");
+                break;
+            case 2:
+                System.out.println(amount + " Dollar = " + (amount * 97.26) + " Ruble");
+                System.out.println(amount + " Dollar = " + (amount * 0.91745) + " Euro");
+                System.out.println(amount + " Dollar = " + (amount * 7.11) + " Yuan");
+                System.out.println(amount + " Dollar = " + (amount * 34.21) + " Lyra");
+                break;
+            case 3:
+                System.out.println(amount + " Euro = " + (amount * 106.08) + " Ruble");
+                System.out.println(amount + " Euro = " + (amount * 1.09) + " Dollar");
+                System.out.println(amount + " Euro = " + (amount * 7.75) + " Yuan");
+                System.out.println(amount + " Euro = " + (amount * 37.28) + " Lyra");
+                break;
+            case 4:
+                System.out.println(amount + " Yuan = " + (amount * 13.59) + " Ruble");
+                System.out.println(amount + " Yuan = " + (amount * 0.140607) + " Dollar");
+                System.out.println(amount + " Yuan = " + (amount * 0.129) + " Euro");
+                System.out.println(amount + " Yuan = " + (amount * 4.81) + " Lyra");
+                break;
+            case 5:
+                System.out.println(amount + " Lyra = " + (amount * 2.84) + " Ruble");
+                System.out.println(amount + " Lyra = " + (amount * 0.029234) + " Dollar");
+                System.out.println(amount + " Lyra = " + (amount * 0.026821) + " Euro");
+                System.out.println(amount + " Lyra = " + (amount * 0.207911) + " Yuan");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }      
         
     }
 
