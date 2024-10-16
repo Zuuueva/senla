762ff6aa579f679299257c6edee669c684f996a0
Natalia <natmizueva@gmail.com>
2024-10-16 19:43:40 +0300

    vers1

--- Task/FirstTask.java
+++ Task/FirstTask.java
@@ -1,5 +1,24 @@
 package Task;
 
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class FirstTask {
+    public static void main(String[] args) {
+        System.out.println("Калькулятор запустился.");
+        var x = List.of("картошка", "ветер", "мука", "ёмкость", "нора");
+        Random randomizer = new Random();
+        String random = x.get(randomizer.nextInt(x.size()));
+        var lives = 5;
+        var guessed = 0;
+        while(lives > 0 || guessed < x.size()){
+            System.out.println("Введите букву:");
+            Scanner s = new Scanner(System.in);
+            char a = s.next().charAt(0);
+            System.out.println("Вы ввели букву:" + a);
+        }
+        
+    }
 
 }
