0e3d6b4112ee90e63501fd3b262245f224eebfaa
Natalia <natmizueva@gmail.com>
2024-10-21 21:52:47 +0300

    change

--- Task/FirdTask.java
+++ Task/FirdTask.java
@@ -7,17 +7,31 @@ public class FirdTask {
     public static void main(String[] args) {
         System.out.println("Введите длину пароля от 8 до 12 символов:");
             Scanner s = new Scanner(System.in);
-            int length = s.nextInt();
-        if (length >= 8 && length <= 12){
-            String characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*_=+-/.?<>~)";
-            SecureRandom rand = new SecureRandom();
-            StringBuilder stringbuild = new StringBuilder();
-            for (int i = 0; i < length; i++)
-            {
-                int randomIndex = rand.nextInt(characters.length());
-                stringbuild.append(characters.charAt(randomIndex));
+            //int length = s.nextInt();
+        int choice = 0;
+        
+        if (s.hasNextInt()) {
+            choice = s.nextInt();
+            System.out.println("Спасибо! Вы ввели число " + choice);
+            if(choice < 8 || choice > 12){
+                System.out.println("К сожалению, такой позиции нет.");
+                return;
             }
-            System.out.println(stringbuild.toString());
-    }
+            else{
+                String characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*_=+-/.?<>~)";
+                SecureRandom rand = new SecureRandom();
+                StringBuilder stringbuild = new StringBuilder();
+                for (int i = 0; i < choice; i++)
+                {
+                    int randomIndex = rand.nextInt(characters.length());
+                    stringbuild.append(characters.charAt(randomIndex));
+                }
+                System.out.println("Ваш пароль: " + stringbuild.toString());
+            }
+        } 
+        else {
+            System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+            return;
+        }
     }
 }
